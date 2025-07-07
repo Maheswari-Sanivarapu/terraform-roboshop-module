0861da7c88fe1e0121d2eb156d6902aeeccdd929
maheswari <mahi.sgs22@gmail.com>
2025-07-08 00:03:21 +0530

    roboshop module

--- locals.tf
+++ locals.tf
@@ -3,13 +3,13 @@ locals {
     private_subnet_id = split(",",data.aws_ssm_parameter.private_subnet_id.value)[0]
     private_subnet_ids = split(",",data.aws_ssm_parameter.private_subnet_id.value)
     tg_port = "${var.component}" == "frontend" ? 80 : 8080
-    health = "${var.component}" == "frontend" ? "/" : "/health"
+    health = "${var.component}" == "frontend" ? "/" : "/health" 
     sg_id = data.aws_ssm_parameter.sg_id.value
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
     alb_listener = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
-    frontend_domain_name = "${var.component}.${var.environment}.${var.route53_domain_name}"
+    frontend_domain_name = "${var.environment}.${var.route53_domain_name}"
     backend_domain_name = "${var.component}.backend-${var.environment}.${var.route53_domain_name}"
     domain_name = "${var.component}" == "frontend" ? local.frontend_domain_name : local.backend_domain_name
     common_tags = {
