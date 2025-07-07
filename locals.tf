85bb18eeb7f17715a521cc5ce2c88464255b93ba
maheswari <mahi.sgs22@gmail.com>
2025-07-07 19:21:32 +0530

    roboshop module

--- locals.tf
+++ locals.tf
@@ -8,7 +8,7 @@ locals {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
-    alb_listener = "${var.component}" == "frontend" ? frontend_alb_listener_arn : backend_alb_listener_arn
+    alb_listener = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
     common_tags = {
         project = var.project
         environment = var.environment
